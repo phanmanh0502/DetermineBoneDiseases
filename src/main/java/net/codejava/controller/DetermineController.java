5dd8a6f55e6aeb283e888b773031de3ac3822164
phanmanh <phanvanmanh1996@gmail.com>
2021-10-24 21:52:57 +0700

    ManhPV comit DetermineController

--- src/main/java/net/codejava/controller/DetermineController.java
+++ src/main/java/net/codejava/controller/DetermineController.java
@@ -1,10 +1,16 @@
 package net.codejava.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import net.codejava.service.DetermineService;
+
 @Controller
 public class DetermineController {
+	@Autowired
+	private DetermineService determineService;
+	
 	@RequestMapping("/determine")
 	public String viewHomePage() {
 		return "determine/determine";
