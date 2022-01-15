8e88a8793cb8223da664fe54ed1acdfb2a9da894
duchv <duchv@hblab.vn>
2022-01-16 02:01:22 +0700

    Add more comments

--- src/main/java/net/codejava/service/DetermineService.java
+++ src/main/java/net/codejava/service/DetermineService.java
@@ -28,27 +28,37 @@ public class DetermineService {
 	 * param: listSymptom danh sách triệu chứng và độ tương đồng, return: listDisease danh sách bệnh và phần trăm mắc mỗi bệnh
 	 */
 	public List<Disease> cbrAlg(List<Symptom> listSymptom) {
-		// Truy vấn list bệnh có khả năng mắc trong các triệu chứng đã nhập
 		Map<Integer, Symptom> mapSymptomIdAndSymptom = new HashMap<>();
 		listSymptom.forEach(symptom -> mapSymptomIdAndSymptom.put(symptom.getId(), symptom));
+
+		// Lấy list id của bệnh từ bảng trọng số với điều kiện có id triệu trứng nằm trong các id triệu trứng gửi lên
 		List<Integer> listDiseaseId = weightService.getListDiseaseIdBySymptomIdIn(mapSymptomIdAndSymptom.keySet());
+
+		// Truy vấn list bệnh có khả năng mắc trong các triệu chứng đã nhập
 		List<Disease> listDisease = diseaseService.getAllByIdIn(listDiseaseId);
+
 		// Tính toán khả năng mắc mỗi bệnh dựa vào thuật toán cbr
+		// Tổng xích ma của ((các trọng số X độ tương đồng tương ứng) / tổng các trọng số)
 		return listDisease.stream().map(disease -> {
 			double percentage = 0;
+
+			// Lấy danh sách các trọng số của 1 bệnh và tính tổng các trọng số
 			List<Weight> weights = disease.getListWeight();
 			double sumWeightOfSymptom = weights.stream().mapToDouble(Weight::getWeightOfSymptom).sum();
 
 			for (Weight weight : weights) {
 				Integer symptomId = weight.getIdSymptom();
+
+				// kiểm tra nếu id triệu trứng nằm trong trọng số có tồn tại trong list triệu trứng gửi lên
 				if (mapSymptomIdAndSymptom.containsKey(symptomId)) {
 					Symptom symptom = mapSymptomIdAndSymptom.get(symptomId);
 					percentage += weight.getWeightOfSymptom() * symptom.getSimilarity() / sumWeightOfSymptom;
 				}
 			}
+
 			percentage *= 100;
-			DecimalFormat f = new DecimalFormat("##.00");
-			percentage = Double.parseDouble(f.format(percentage));
+			DecimalFormat f = new DecimalFormat("#.##");
+			percentage = Double.parseDouble(f.format(percentage).replace(",", "."));
 			disease.setPercentage(percentage);
 			return disease;
 		// Sắp xếp phần trăm mắc bệnh
